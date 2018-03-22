cff68f7dd22e21f36f5142159f6ff0f8da45abbf
Create Unity Project <ryohei@R-NakanonoMacBook-Pro.local>
2018-03-22 21:25:55 +0900

    ・コメントアウト追加 ・効果音削除

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,17 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;//シーン遷移に必要！
 
 public class PlayerScript : MonoBehaviour {
-	[SerializeField] float speed = 6f;
-	[SerializeField] int hp = 3;
-	AudioClip damageSound;
-	AudioSource audioSource;
+	
+	float speed = 10.0f;//Playerの移動スピード
+
 	// Use this for initialization
 	void Start () {
-		damageSound = Resources.Load <AudioClip>("Sounds/Sound1");
-		audioSource = GetComponent<AudioSource> ();
+
 	}
 	
 	// Update is called once per frame
@@ -19,41 +17,34 @@ public class PlayerScript : MonoBehaviour {
 		Move ();//Playerを移動させる関数
 	}
 
-	//====HP実装ありの場合====
-//	void OnTriggerEnter(Collider other){
-//		if(other.gameObject.tag == "Enemy"){
-//			if (hp > 1) {
-//				audioSource.PlayOneShot (damageSound);
-//				hp--;
-//			} else {
-//				SceneManager.LoadScene ("GameOverScene");
-//			}
-//
-//		}
-//	}
-
-	//====HP実装無しの場合====
+	//当たり判定検知(isTriggerにチェック入れる！)
 	void OnTriggerEnter(Collider other){
+
+		//当たった相手のtagがEnemyだった時
 		if(other.gameObject.tag == "Enemy"){
-			SceneManager.LoadScene ("GameOverScene");
+			SceneManager.LoadScene ("GameOverScene");//GameOverSceneにシーン遷移
 		}
 	}
 
 	//Playerを移動させる関数
 	void Move()
 	{
+		//右キー押した時
 		if(Input.GetKey("up") == true){
 			this.transform.position += new Vector3(0, 0, speed * Time.deltaTime);//"this"てのはアタッチしてるオブジェクト自身のこと
 		}
 
+		//下キー押した時
 		if(Input.GetKey("down") == true){
 			this.transform.position += new Vector3(0, 0, -speed * Time.deltaTime);//"this"てのはアタッチしてるオブジェクト自身のこと
 		}
 
+		//右キーした時
 		if(Input.GetKey("right") == true){
 			this.transform.position += new Vector3(speed * Time.deltaTime, 0, 0);//"this"てのはアタッチしてるオブジェクト自身のこと
 		}
 
+		//左キー押した時
 		if(Input.GetKey("left") == true){
 			this.transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);//"this"てのはアタッチしてるオブジェクト自身のこと
 		}
